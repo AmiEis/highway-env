2aa6c672cfdf687a511fd6256c2c9c48bb19a6bc
AMICHAI EISENMANN <AMIHAIA@rafael.co.il>
2022-03-08 10:20:06 +0200

    move test

--- scripts/test.py
+++ scripts/test.py
@@ -1,18 +1,17 @@
 import gym
 import highway_env.envs.highway_env_scene
-from highway_env.envs.highway_env_scene import HighwayEnv
+from highway_env.envs.highway_env_scene import HighwayEnvFast
 import sys
-import warnings
-warnings.simplefilter(action='ignore', category=FutureWarning)
+from configs import get_config
 
 if __name__ == "__main__":
     #env = gym.make("highway-v0")
     #config = {"action":{"type":"DiscreteAction"}}
     #config = {"action": {"type": "ContinuousAction"}}
-    env = HighwayEnv()
+    env = HighwayEnvFast(get_config())
     env.config["lanes_count"] = 3
     obs = env.reset()
-    for i in range(200):
+    for i in range(1000):
         '''if i % 10 == 0:
             action = 1
         elif i % 20 == 0:
@@ -33,6 +32,7 @@ if __name__ == "__main__":
         action = 1
         obs, rew, done, info = env.step(action)
         env.render()
+        print(env)
         if done: env.reset()
 
 
