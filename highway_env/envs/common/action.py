9013256a3d8fd63bb99736ececdf47b165400c7c
AMICHAI EISENMANN <AMIHAIA@rafael.co.il>
2022-03-10 09:02:36 +0200

    Fix discrete action bug

--- highway_env/envs/common/action.py
+++ highway_env/envs/common/action.py
@@ -156,7 +156,7 @@ class DiscreteAction(ContinuousAction):
     def act(self, action: int) -> None:
         cont_space = super().space()
         axes = np.linspace(cont_space.low, cont_space.high, self.actions_per_axis)
-        all_actions = list(product(axes))
+        all_actions = list(product(axes[:,0],axes[:,1]))
         super().act(all_actions[action])
 
 
