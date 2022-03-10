0ad6b362de6a337b32a70da4e19290cd09a0cc34
AMICHAI EISENMANN <AMIHAIA@rafael.co.il>
2022-03-10 09:05:57 +0200

    Save checkpoints

--- scripts/main.py
+++ scripts/main.py
@@ -17,7 +17,6 @@ from highway_env.envs.highway_env_scene import HighwayEnvFast
 from configs import get_config
 
 n_envs = 16
-n_stack = 4
 tensorboard_log = "highway_ppo/"
 Alg = "PPO"
 
@@ -29,7 +28,7 @@ if __name__ == "__main__":
                 verbose=0,
                 n_steps=512,
                 target_kl=0.015,
-                gamma=0.99,
+                gamma=0.95,
                 gae_lambda=0.9,
                 ent_coef=0.015,
                 tensorboard_log=tensorboard_log,
@@ -38,12 +37,12 @@ if __name__ == "__main__":
     datetimestr = datetime.now().strftime('%d-%m-%Y')
     #checkpoint_dir = Alg + "_" + datetimestr
     #base_dir = os.path.join(r"D:\projects\RL\highway-env\models", checkpoint_dir)
-    models_dir = r"D:\projects\RL\highway-env\models"
-    model.learn(20_000)
-    model.save(os.path.join(models_dir,Alg+"_"+datetimestr))
-    #if not os.path.exists(base_dir):
-    #    os.makedirs(base_dir)
-    '''for i in range(8):
-        model.learn(1_000_000, reset_num_timesteps=True if i == 0 else False)
-        model.save(os.path.join(base_dir, "no_" + str(i)))
-        '''
+    models_dir = os.path.join("D:\projects\RL\highway-env\models",Alg+"_"+datetimestr)
+    if not os.path.exists(models_dir):
+        os.makedirs(models_dir)
+    for i in range(8):
+        model.learn(1_000_000,reset_num_timesteps=True if i==0 else False)
+        model.save(os.path.join(models_dir, Alg+"_"+datetimestr, "no_" + str(i)))
+    #model.learn(2_000_000)
+    #model.save(os.path.join(models_dir,Alg+"_"+datetimestr))
+
