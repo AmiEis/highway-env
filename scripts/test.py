4c2cbfb77cfad794ec6442cb1a4601884e25003c
AMICHAI EISENMANN <AMIHAIA@rafael.co.il>
2022-03-20 16:27:54 +0200

    test narrow pass

--- scripts/test.py
+++ scripts/test.py
@@ -8,10 +8,10 @@ import cv2 as cv
 import matplotlib.pyplot as plt
 from highway_env.road.my_image_renderer import MyImageRenderer
 import random
-
-
-
-
+import time
+from highway_env.envs.lane_keeping_env import LaneKeepingEnv
+from highway_env.envs.merge_env import MergeEnv
+from highway_env.envs.narrow_pass import NarrowPass
 
 if __name__ == "__main__":
     #env = gym.make("highway-v0")
@@ -20,49 +20,38 @@ if __name__ == "__main__":
     env = HighwayEnvFast(get_config(is_test=False))
     #env.config["action"] = {"type":"DiscreteAction", "actions_per_axis":3}
     env.config["vehicles_density"] = 1
+    env.config["screen_width"] = 256
+    env.config["screen_height"] = 64
+    env.config["scaling"] = 2.5
+    env = NarrowPass({
+        "observation": {
+            "type": "GrayscaleObservation",
+            #"type": "MyImageObservation",
+            "observation_shape": (256, 64),
+            "stack_size": 4,
+            "weights": [0.2989, 0.5870, 0.1140],  # weights for RGB conversion
+            "scaling": 2.5,
+        },
+    })
     obs = env.reset()
-    myImageRenderer = MyImageRenderer(env)
-
-    #env.render()
+    #myImageRenderer = MyImageRenderer(env)
+    env.render()
     for i in range(10000):
-        '''if i % 10 == 0:
-            action = 1
-        elif i % 20 == 0:
-            action = 2
-        else:
-            action = 1'''
-
-        '''if i % 5 == 0:
-            action = (0,0.1)
-        elif i % 5 == 1:
-            action = (0,-0.1)
-        elif i % 5 == 2:
-            action = (0,-0.1)
-        elif i % 5 == 3:
-            action = (0,0.1)
-        else:
-            action = (0,0)'''
-        action = 3#random.choice([1,3,4])
-        # if i % 100 == 0:
-        #    action = np.random.randint(9)
-
-
-         # if i % 100 == 0:
-         #    s = np.random.randint(2)
-         #    action = 0 if s == 0 else 2
+        action = 1#[0,0]#random.choice([1,3,4])
+        # if i % 10 == 0: action = random.choice([0,2])
         obs, rew, done, info = env.step(action)
-        #env.render()
-        myImageRenderer.my_render()
+        env.render()
+        #myImageRenderer.my_render()
+        time.sleep(0.2)
         plt.pause(0.0001)
-        '''speeds = [v.speed for v in env.road.vehicles]
-        target_speeds = [v.target_speed for v in env.road.vehicles]
+        '''target_speeds = [v.target_speed for v in env.road.vehicles]
         positions = [v.position[0] for v in env.road.vehicles]
         print(speeds)
         print(target_speeds)
         print(positions)'''
         if done:
             env.reset()
-            myImageRenderer.reset_pos()
+            # myImageRenderer.reset_pos()
             print('done')
 
 
