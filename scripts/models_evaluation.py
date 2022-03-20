31cf44dd8eddf7ac9e81be311749286582c1659f
AMICHAI EISENMANN <AMIHAIA@rafael.co.il>
2022-03-20 10:09:00 +0200

    Add option to save video

--- scripts/models_evaluation.py
+++ scripts/models_evaluation.py
@@ -7,28 +7,38 @@ import time
 from configs import get_config
 from highway_env.road.my_image_renderer import MyImageRenderer
 import matplotlib.pyplot as plt
+import cv2 as cv
+from datetime import datetime
+import os
 
 
 if __name__ == "__main__":
     #model = PPO.load("../models/PPO_06-03-2022")
-    model = PPO.load(r"D:\projects\RL\highway-env\models\PPO_07-03-2022.zip")
-    #model = PPO.load(r"D:\projects\RL\highway-env\models\PPO_10-03-2022\no_7.zip")
+    #model = PPO.load(r"D:\projects\RL\highway-env\models\PPO_07-03-2022.zip")
+    model = PPO.load(r"D:\projects\RL\highway-env\models\PPO_16-03-2022\no_7.zip")
+    Save = False
+    datetimestr = datetime.now().strftime('%d-%m-%Y')
+    image_folder = r"D:\projects\RL\highway-env\results\\"+datetimestr+r"\\"
+    if not os.path.exists(image_folder):
+        os.makedirs(image_folder)
 
+    Show = False
     #env = gym.make("highway-fast-v0", config=config)
     #env = highway_env.envs.HighwayEnvFast(config)
-    env = highway_env.envs.HighwayEnvFast(get_config(is_test=False))
-    env.seed(123)
+    env = highway_env.envs.HighwayEnvFast(get_config(is_test=True))
+    env.seed(1234)
     env.reset()
     myImageRenderer = MyImageRenderer(env)
     n_collisions = 0
     n_off_road = 0
     n_success = 0
-    n_scenes = 10
+    n_scenes = 100
     mean_speed = 0
     cnt = 0
     cnt_mean_speed_close_to_target_5_pct = 0
     cnt_mean_speed_close_to_target_10_pct = 0
     speeds = []
+    i = 0
     for _ in range(n_scenes):
         start_reset = time.perf_counter()
         obs = env.reset()
@@ -38,6 +48,7 @@ if __name__ == "__main__":
         step_times = []
         target_speeds = [v.target_speed for v in env.road.vehicles]
         #print(env.road.vehicles[2].target_speed,env.road.vehicles[2].speed)
+        speeds = [v.speed for v in env.road.vehicles]
         while not done:
             action, _ = model.predict(obs,deterministic=True)
             ego_speed = env.controlled_vehicles[0].speed
@@ -49,6 +60,11 @@ if __name__ == "__main__":
             obs, reward, done, info = env.step(action)
             end_step = time.perf_counter()
             #print('step time = ', end_step - start_step)
+            # new_speeds = [v.speed for v in env.road.vehicles]
+            # for i, (s_o, s_n) in enumerate(zip(new_speeds, speeds)):
+            #     if s_n - s_o < -(1.0 / 3) * 3.5:
+            #         print('v {} decelarted from {} to {}'.format(i, s_o, s_n))
+            # speeds = new_speeds
             step_times.append(end_step - start_step)
             if env.vehicle.crashed:
                 n_collisions += 1
@@ -56,11 +72,28 @@ if __name__ == "__main__":
                 n_success += 1
             if env.config["offroad_terminal"] and not env.vehicle.on_road:
                 n_off_road += 1
-            myImageRenderer.my_render()
-            #env.render()
-            plt.pause(0.0001)
-            #time.sleep(0.05)
+            # start = time.perf_counter()
+            image = myImageRenderer.my_render()
+            # plt.pause(0.0001)
+            # end = time.perf_counter()
+            # print('rendering: ',end-start)
+            if Show:
+                plt.imshow(im)
+                plt.pause(0.001)
+                #env.render()
+                time.sleep(0.01)
+            if Save:
+                imname = str(i)
+                while len(imname) < 4:
+                    imname = '0' + imname
+                image = np.dstack(((image * 150) % 255, (image * 333) % 255, (image * 98) % 255)).astype(np.uint8)
+                #image = cv.resize(image, (160, 480), interpolation=cv.INTER_NEAREST)
+                # cv.imwrite(r"D:\projects\RL\Avatar\log\images\result\\" + imname + ".png", image)
+                cv.imwrite(image_folder + imname + ".png", image)
+                i += 1
+
             # if done:
+            #     myImageRenderer.reset_pos()
             #     print("done")
             #for i,v in enumerate(env.road.vehicles):
             #    if i > 0 and v.speed > target_speeds[i]:
