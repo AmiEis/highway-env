a2b3ca1c2f203583515e3b169317ac7214f9166a
AMICHAI EISENMANN <AMIHAIA@rafael.co.il>
2022-03-20 10:08:06 +0200

    add option to run DQN

--- scripts/main.py
+++ scripts/main.py
@@ -1,12 +1,12 @@
 import gym
 import torch as th
-from stable_baselines3 import PPO
+from stable_baselines3 import PPO, DQN
 from torch.distributions import Categorical
 import torch
 import torch.nn as nn
 import numpy as np
 from torch.nn import functional as F
-from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.env_util import make_vec_env, DummyVecEnv
 from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
 from stable_baselines3.common.vec_env import SubprocVecEnv
 import highway_env
@@ -15,34 +15,62 @@ from datetime import datetime
 import os
 from highway_env.envs.highway_env_scene import HighwayEnvFast
 from configs import get_config
+from sys import exit
+from stable_baselines3.common.callbacks import CheckpointCallback
 
 n_envs = 16
-tensorboard_log = "highway_ppo/"
-Alg = "PPO"
+alg = 'DQN'
+tensorboard_log = "highway_{}/".format(alg.lower())
 
 if __name__ == "__main__":
     config = {"config": get_config()}
-    env = make_vec_env(HighwayEnvFast,n_envs=16,env_kwargs=config)
-    model = PPO('CnnPolicy',
-                env,
-                verbose=0,
-                n_steps=512,
-                target_kl=0.015,
-                gamma=0.95,
-                gae_lambda=0.9,
-                ent_coef=0.015,
-                tensorboard_log=tensorboard_log,
-                device='cuda:0')
+    if alg == 'PPO':
+        env = make_vec_env(HighwayEnvFast, n_envs=16, env_kwargs=config)
+        model = PPO('CnnPolicy',
+                    env,
+                    verbose=0,
+                    n_steps=512,
+                    target_kl=0.015,
+                    gamma=0.95,
+                    gae_lambda=0.9,
+                    ent_coef=0.015,
+                    tensorboard_log=tensorboard_log,
+                    device='cuda:0')
+    elif alg == 'DQN':
+
+        model = DQN('CnnPolicy',
+                    DummyVecEnv([lambda: HighwayEnvFast(config=get_config())]),
+                    learning_rate=1e-4,
+                    buffer_size=200_000,
+                    learning_starts=1000,
+                    batch_size=32,
+                    gamma=0.95,
+                    train_freq=1,
+                    gradient_steps=1,
+                    target_update_interval=50,
+                    exploration_fraction=0.4,
+                    verbose=0,
+                    tensorboard_log=tensorboard_log)
+    else:
+        exit("Alg \'{}\' not supported!".format(alg))
+
 
     datetimestr = datetime.now().strftime('%d-%m-%Y')
+    #model = PPO.load(r"D:\projects\RL\highway-env\models\PPO_16-03-2022\no_0.zip",env)
     #checkpoint_dir = Alg + "_" + datetimestr
     #base_dir = os.path.join(r"D:\projects\RL\highway-env\models", checkpoint_dir)
-    models_dir = os.path.join("D:\projects\RL\highway-env\models",Alg+"_"+datetimestr)
+    models_dir = os.path.join("D:\projects\RL\highway-env\models",alg+"_"+datetimestr)
     if not os.path.exists(models_dir):
-        os.makedirs(models_dir)
-    for i in range(8):
-        model.learn(1_000_000,reset_num_timesteps=True if i==0 else False)
-        model.save(os.path.join(models_dir, "no_" + str(i)))
+            os.makedirs(models_dir)
+
+    # Save a checkpoint every save_freq steps
+    save_freq = 1_000_000
+    checkpoint_callback = CheckpointCallback(save_freq=save_freq, save_path=models_dir,
+                                             name_prefix=alg)
+    model.learn(8_000_000,callback=checkpoint_callback)
+    # for i in range(1,8):
+    #     model.learn(1_000_000,reset_num_timesteps=True if i==0 else False)
+    #     model.save(os.path.join(models_dir, "no_" + str(i)))
     #model.learn(2_000_000)
     #model.save(os.path.join(models_dir,Alg+"_"+datetimestr))
 
