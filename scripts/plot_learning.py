4e9c39e05ca0c000032836431539988698723ba7
AMICHAI EISENMANN <AMIHAIA@rafael.co.il>
2022-04-10 15:09:38 +0300

    fix bug. save values to text file in addition

--- scripts/plot_learning.py
+++ scripts/plot_learning.py
@@ -10,7 +10,7 @@ from os.path import join, exists
 from os import makedirs
 
 env = highway_env.envs.HighwayEnvFast(get_config(is_test=False))
-action_type = 'DiscreteMetaAction'
+action_type = 'DiscreteAction'
 env.config["action"]["type"] = action_type
 n_scenes = 200
 success_rate = []
@@ -26,8 +26,10 @@ if __name__ == "__main__":
     image_folder = r"D:\projects\RL\highway-env\results\graphs\\" + datestr + r"\\"
     if not exists(image_folder):
         makedirs(image_folder)
+    model_files = listdir(base_dir)
+    model_files.sort(key=lambda fn: int(fn.split('.')[0].split('_')[1]))
 
-    for i,fn in enumerate(listdir(base_dir)):
+    for i,fn in enumerate(model_files):
         model = PPO.load(join(base_dir, fn))
         env.seed(1234)
         env.reset()
@@ -109,3 +111,11 @@ if __name__ == "__main__":
     #axs[1].set(xlabel='M learning steps', ylabel='at 10% of target speed')
     fig.tight_layout()
     plt.savefig(join(image_folder,'fig2.png'))
+    lines = []
+    lines.append(','.join(map(str, success_rate)))
+    lines.append(','.join(map(str, km_per_collision)))
+    lines.append(','.join(map(str, emergency_breaks_pct)))
+    lines.append(','.join(map(str, mean_speeds)))
+    lines = map(lambda l: l + '\n',lines)
+    with open(join(image_folder,'results.txt'),'w') as file:
+        file.writelines(lines)
