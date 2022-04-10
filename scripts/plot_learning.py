6c3b91a3097e786a26291f80e2bb5cffc50319eb
AMICHAI EISENMANN <AMIHAIA@rafael.co.il>
2022-04-10 08:17:45 +0300

    change plots - measure mean speed through all episodes

--- scripts/plot_learning.py
+++ scripts/plot_learning.py
@@ -8,26 +8,28 @@ from configs import get_config
 from os import listdir
 from os.path import join, exists
 from os import makedirs
-from datetime import datetime
 
-
-datetimestr = datetime.now().strftime('%d-%m-%Y')
-image_folder = r"D:\projects\RL\highway-env\results\graphs\\"+datetimestr+r"\\"
-if not exists(image_folder):
-    makedirs(image_folder)
 env = highway_env.envs.HighwayEnvFast(get_config(is_test=False))
-env.seed(1234)
+action_type = 'DiscreteMetaAction'
+env.config["action"]["type"] = action_type
 n_scenes = 200
 success_rate = []
 km_per_collision = []
+mean_speeds = []
 ego_speed_at_5pct = []
 ego_speed_at_10pct = []
 emergency_breaks_pct = []
 
 if __name__ == "__main__":
-    base_dir = r"D:\projects\RL\highway-env\models\PPO_22-03-2022"
+    base_dir = r"D:\projects\RL\highway-env\models\PPO_07-04-2022"
+    datestr = base_dir.split('\\')[-1].split('_')[-1]
+    image_folder = r"D:\projects\RL\highway-env\results\graphs\\" + datestr + r"\\"
+    if not exists(image_folder):
+        makedirs(image_folder)
+
     for i,fn in enumerate(listdir(base_dir)):
         model = PPO.load(join(base_dir, fn))
+        env.seed(1234)
         env.reset()
         n_collisions = 0
         n_off_road = 0
@@ -68,35 +70,42 @@ if __name__ == "__main__":
                 if done:
                     pos_end = env.vehicle.position[0]
                     distance_passed += pos_end - pos_start
-            if np.abs(mean_speed - 30)/30 < 0.05:
-                cnt_mean_speed_close_to_target_5_pct += 1
-            if np.abs(mean_speed - 30) / 30 < 0.1:
-                cnt_mean_speed_close_to_target_10_pct += 1
-            cnt = 0
+            #if np.abs(mean_speed - 30)/30 < 0.05:
+            #    cnt_mean_speed_close_to_target_5_pct += 1
+            #if np.abs(mean_speed - 30) / 30 < 0.1:
+            #    cnt_mean_speed_close_to_target_10_pct += 1
+        cnt = 0
         success_rate.append(float(n_success)/n_scenes)
         km_per_collision.append((distance_passed/1000)/max(float(n_collisions),1e-16))
-        ego_speed_at_5pct.append(float(cnt_mean_speed_close_to_target_5_pct)/n_scenes)
-        ego_speed_at_10pct.append(float(cnt_mean_speed_close_to_target_10_pct)/n_scenes)
+        mean_speeds.append(mean_speed)
+        #ego_speed_at_5pct.append(float(cnt_mean_speed_close_to_target_5_pct)/n_scenes)
+        #ego_speed_at_10pct.append(float(cnt_mean_speed_close_to_target_10_pct)/n_scenes)
         emergency_breaks_pct.append(float(rear_breaking_n)/max(float(lane_changes_n),1e-16))
-    ran = range(len(listdir(base_dir)))
-    fig, axs = plt.subplots(3,1)
+    ran = range(1,len(listdir(base_dir))+1)
+    fig, axs = plt.subplots(2,1)
     axs[0].plot(ran, success_rate)
     axs[0].set_title('Success rate %')
     axs[0].set(xlabel='M learning steps', ylabel='Success rate')
     axs[1].plot(ran, km_per_collision)
     axs[1].set_title('KM per Collision')
     axs[1].set(xlabel='M learning steps', ylabel='KM per Collision')
-    axs[2].plot(ran, emergency_breaks_pct)
-    axs[2].set_title('Emergency Breaks % at Lane Change')
-    axs[2].set(xlabel='M learning steps', ylabel='Emergency Breaks')
+    #axs[2].plot(ran, emergency_breaks_pct)
+    #axs[2].set_title('Emergency Breaks % at Lane Change')
+    #axs[2].set(xlabel='M learning steps', ylabel='Emergency Breaks')
     fig.tight_layout()
     plt.savefig(join(image_folder,'fig1.png'))
     fig, axs = plt.subplots(2,1)
-    axs[0].plot(ran, ego_speed_at_5pct)
-    axs[0].set_title('Rate at 5% of target speed')
-    axs[0].set(xlabel='M learning steps', ylabel='at 5% of target speed')
-    axs[1].plot(ran, ego_speed_at_10pct)
-    axs[1].set_title('Rate at 10% of target speed')
-    axs[1].set(xlabel='M learning steps', ylabel='at 10% of target speed')
+    axs[0].plot(ran, emergency_breaks_pct)
+    axs[0].set_title('Emergency Breaks % at Lane Change')
+    axs[0].set(xlabel='M learning steps', ylabel='Success rate')
+    axs[1].plot(ran, mean_speeds)
+    axs[1].set_title('Mean Speed (target speed = 30)')
+    axs[1].set(xlabel='M learning steps', ylabel='mean speed')
+    #axs[0].plot(ran, ego_speed_at_5pct)
+    #axs[0].set_title('Rate at 5% of target speed')
+    #axs[0].set(xlabel='M learning steps', ylabel='at 5% of target speed')
+    #axs[1].plot(ran, ego_speed_at_10pct)
+    #axs[1].set_title('Rate at 10% of target speed')
+    #axs[1].set(xlabel='M learning steps', ylabel='at 10% of target speed')
     fig.tight_layout()
     plt.savefig(join(image_folder,'fig2.png'))
